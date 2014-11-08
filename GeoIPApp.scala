f7b956fe4c182957c0594fbefebce96456e0009b
Kpacha <kpacha666@gmail.com>
2014-11-08 19:18:44 +0100

    initial commit

---
+++ GeoIPApp.scala
@@ -0,0 +1,63 @@
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import MediaTypes._
+
+class GeoIPServiceActor extends Actor with GeoIPService {
+  def actorRefFactory = context
+  def receive = runRoute(myRoute)
+}
+
+import spray.json._
+import spray.json.DefaultJsonProtocol
+import com.snowplowanalytics.maxmind.iplookups.IpLocation
+
+object IpLocationJsonProtocol extends DefaultJsonProtocol {
+  implicit val ipLocationFormat = jsonFormat12(IpLocation.apply)
+}
+
+import IpLocationJsonProtocol._
+import com.snowplowanalytics.maxmind.iplookups.IpLookups
+import scala.concurrent.duration.Duration
+import spray.routing.directives.CachingDirectives._
+
+trait GeoIPService extends HttpService {
+
+  val ipLookups = {
+    println("ipLookups instantion. Reading data from /tmp/GeoLiteCity.dat")
+    IpLookups(geoFile = Some("/tmp/GeoLiteCity.dat"), ispFile = None,
+              orgFile = None, domainFile = None, memCache = true, lruCache = 20000)
+  }
+
+  val simpleCache = routeCache(maxCapacity = 1000, timeToIdle = Duration("30 min"))
+
+  val myRoute =
+    path("geoip" / """\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b""".r) { ip =>
+      get {
+        cache(simpleCache) {
+          respondWithMediaType(`application/json`) {
+            complete {
+              ipLookups.performLookups(ip)._1 match {
+                case Some(ipLocation) => ipLocation.toJson.toString
+                case _ => "null"
+              }
+            }
+          }
+        }
+      }
+    }
+}
+
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+object GeoIPApp extends App {
+  implicit val system = ActorSystem("system-actor")
+  val service = system.actorOf(Props[GeoIPServiceActor], "geoip-service")
+  implicit val timeout = Timeout(5.seconds)
+  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+}
---
+++ build.sbt
@@ -0,0 +1,27 @@
+name := "akka-geoip"
+
+version := "0.0.1-SNAPSHOT"
+
+scalaVersion := "2.10.3"
+
+libraryDependencies ++={
+  val akkaV = "2.3.6"
+  val sprayV = "1.3.2"
+  Seq(
+    "io.spray"            %%  "spray-can"     % sprayV,
+    "io.spray"            %%  "spray-caching" % sprayV,
+    "io.spray"            %%  "spray-routing" % sprayV,
+    "io.spray"            %%  "spray-json"    % "1.3.1",
+    "io.spray"            %%  "spray-testkit" % sprayV  % "test",
+    "com.typesafe.akka"   %%  "akka-actor"    % akkaV,
+    "com.typesafe.akka"   %%  "akka-testkit"  % akkaV   % "test",
+    "com.snowplowanalytics"  %% "scala-maxmind-iplookups"  % "0.2.0",
+    "org.specs2"          %%  "specs2-core"   % "2.3.7" % "test"
+  )
+}
+
+resolvers ++= Seq(
+  "Twitter" at "http://maven.twttr.com",
+  "SnowPlow Repo" at "http://maven.snplow.com/releases/",
+  "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+)
